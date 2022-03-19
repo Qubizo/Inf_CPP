a1fbcc669f6e39b5a285b45f1d05306b7123061e
Szymon <szymonzazulak116@gmail.com>
2022-03-19 09:30:30 +0100

    Add code Inf_CPP

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Wpisz 1 liczbę całkowita:";
+    
+    int liczba1;
+    cin >> liczba1;
+    
+    cout << "Wpisz 2 liczbę całkowita:";
+    
+    int liczba2;
+    cin >> liczba2;
+    
+    cout << "Wynik dodawania to: ";
+    cout << liczba1 + liczba2;
+
+    return 0;
+}
